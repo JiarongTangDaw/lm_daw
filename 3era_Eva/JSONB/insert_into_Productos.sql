af077e1f0657a5124214e13d9ea5d592b29e065d
Jiarong Tang <57526773+jiarongTang@users.noreply.github.com>
2025-04-23 12:39:18 +0200

    LM-postgreed+json

---
+++ 3era_Eva/JSONB/insert_into_Productos.sql
@@ -0,0 +1,23 @@
+INSERT INTO Productos (nombre,caracteristicas)
+VALUES (
+	'Camiseta manga corta'
+	, '
+		{
+            "marca": "Fast Fish",
+            "color": "Rojo",
+            "talla": "XL",
+            "cantidad": 2
+        }
+	'
+),
+(
+	'Sudadera'
+	, '
+		{
+            "marca": "Fast Fish",
+            "color": "Azul",
+            "talla": "L",
+            "cantidad": 1
+        }
+	'
+);
