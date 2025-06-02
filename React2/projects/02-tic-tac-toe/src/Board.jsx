ef7e8fb9dcf94b52683b0e82840a8e0365a4c5e9
Jiarong Tang <187628173+JiarongTangDaw@users.noreply.github.com>
2025-06-02 14:19:13 +0200

    react - tic-tac-toe

---
+++ React2/projects/02-tic-tac-toe/src/Board.jsx
@@ -0,0 +1,127 @@
+import confetty from 'canvas-confetti';
+
+import { useState } from 'react';
+
+import { Square } from './components/Square';
+import { WinnerModal } from './components/Winner';
+
+import './App.css'
+
+function Board() {
+  //const board = /*Array(9).fill(null);*/["X","O","X","X","O","X","X","O","X"];
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const TURNS = {
+    X: "X",
+    O: "O"
+  }
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const WINNER_COMBOS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,9],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  
+  //el ganador por defecto esta a null, seolo cambia si encontramos un ganador
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for(const combo of WINNER_COMBOS) {
+      const[a,b,c] = combo;
+
+      if(
+        boardToCheck [a] &&
+        boardToCheck [a] === boardToCheck[b] &&
+        boardToCheck [a] === boardToCheck[c]
+      ){
+        return boardToCheck [a]
+      }
+    }
+    
+    // si no hay ganador
+    return null
+  }
+
+  const updateBoard = (index) => {
+    // comprobacion para que no añada un valor sobre una casilla que ya tenga un valor
+    if(board[index] || winner) return 
+
+    //copiamos el array original en una nueva variable, de sta manera no modificamos el componente
+    const newBoard = [...board];
+
+    //logica de actualizacion de tablero
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    // cambiar turno
+    const newTurn = turn === TURNS.X? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if(newWinner){
+      confetty();
+      setWinner(newWinner);
+    }
+    else if (checkEndGame(newBoard)){
+      setWinner(false);
+    }
+  }
+
+  // const handleClick = (index) => {
+  //   updateBoard(index);
+  // }
+
+  // dejamos nuestro estados con los valores iniciales, de esta manera podemos reiniciar el juego
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null);
+  }
+  
+
+  return (
+    <>
+      <div className='board'>
+        <h1>Tic-Tac-Toe</h1>
+        <button onClick={resetGame}>Reiniciar Tablero</button>
+        <section className='game'>
+          {
+            board.map((square, index) => {
+              return (
+                <Square 
+                  key = {index}
+                  index={index}
+                  updateBoard={updateBoard}
+                >
+                  {square}
+                </Square>
+              )
+            })
+          }
+        </section>
+        
+        <section className='turn'>
+          <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+          <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+        </section>
+
+
+        <WinnerModal winner={winner} resetGame={resetGame}/>
+      </div>
+      
+
+    </>
+  )
+}
+
+export default Board
